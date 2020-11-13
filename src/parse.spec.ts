d9c1164dfc680a717f224c50e23efdad3bf9ceed
Jam Risser <jam@codejam.ninja>
2020-11-13 00:40:23 +0000

    Test parse

---
+++ src/parse.spec.ts
@@ -0,0 +1,39 @@
+import { parseQuestions } from './parse';
+import { IQuestion, QuestionType } from './question';
+
+describe('parseQuestions()', () => {
+  it('should parse questions', () => {
+    const questions: IQuestion[] = [
+      {
+        group: 'Config',
+        label: 'hello',
+        type: QuestionType.String,
+        variable: 'hello.enabled',
+        subquestions: [
+          {
+            group: 'Config',
+            label: 'hello',
+            type: QuestionType.String,
+            variable: 'hello.world'
+          }
+        ]
+      }
+    ];
+    expect(parseQuestions(questions)).toMatchObject([
+      {
+        group: 'Config',
+        label: 'hello',
+        type: QuestionType.String,
+        variable: ['hello', 'enabled'],
+        subquestions: [
+          {
+            group: 'Config',
+            label: 'hello',
+            type: QuestionType.String,
+            variable: ['hello', 'world']
+          }
+        ]
+      }
+    ]);
+  });
+});
